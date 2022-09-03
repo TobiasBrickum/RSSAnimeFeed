b690ab9f848074924a3e175b8e293ba6a8277012
Tobi <72128256+TobiasBrickum@users.noreply.github.com>
2022-09-03 02:07:17 +0200

    initial

--- RSSAnimeFeed_Console/RSSAnimeFeed_Console/Program.cs
+++ RSSAnimeFeed_Console/RSSAnimeFeed_Console/Program.cs
@@ -1,2 +1,112 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace HTML_Downlaod_Konsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shiki say: Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+            //TryStackOvebrflow();
+
+            //Test_HTML_ReadInput_WriteInput();
+
+            TestRSSFEED();
+        }
+
+        public static void TestRSSFEED()
+        {
+            RSSLibarie rssrREader = new RSSLibarie();
+            //rssrREader.ReadNewAnimeTitleRss();
+            rssrREader.viewCalendaryWeek("Monday");
+        }
+
+        /// <summary>
+        /// Read HTML file...
+        /// </summary>
+        public static void Test_HTML_ReadInput_WriteInput()
+        {
+            string inputFileName = "https __www.crunchyroll.com_de_fuuto-pi.htm";
+            string outputFileName = "output_file_from_readed_input_file.txt";
+            string bothFilePath = "TestFiles";
+            HTML_Reader readHzml = new HTML_Reader(inputFileName, outputFileName, bothFilePath, bothFilePath);
+
+            List<string> readedInputFile = readHzml.ReadInputFile();
+            readHzml.WriteInputFile(readedInputFile);
+        }
+
+
+
+        public static void Test_HTML_Downlaod_Picture()
+        {
+            String anime = "https://cdn.discordapp.com/attachments/693433288987246642/1008342444456288296/";
+
+            string remoteUri = anime;
+            string fileName = "unknown.png", myStringWebResource = null;
+            //https://cdn.discordapp.com/attachments/693433288987246642/1008342444456288296/unknown.png
+            // Create a new WebClient instance.
+            WebClient myWebClient = new();
+
+
+            // Concatenate the domain with the Web resource filename.
+            myStringWebResource = remoteUri + fileName;
+            Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", fileName, myStringWebResource);
+
+            // Download the Web resource and save it into the current filesystem folder.
+            myWebClient.DownloadFile(myStringWebResource, fileName);
+            Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", fileName, myStringWebResource);
+            //Console.WriteLine("\nDownloaded file saved in the following file system folder:\n\t" + Application.StartupPath);
+        }
+
+        public static void TestDownloadHTML()
+        {
+            string animeUrl = "https://www.crunchyroll.com/de/fuuto-pi";
+            //animeUrl = "https://google.de";
+            string downlaodName = "c.html";
+
+            WebClient wClient = new WebClient();
+            wClient.Headers.Add("User-Agent: Other");   //that is the simple line!
+            wClient.DownloadFile(animeUrl, downlaodName);
+
+            /*
+            string strSource = wClient.DownloadString("https://www.crunchyroll.com/de/fuuto-pi");
+            StreamWriter writer = new StreamWriter("google.html");
+            writer.Write(strSource);
+            writer.Close();
+            */
+            Console.WriteLine("\n\n wurde einfach so aufgerunfen");
+        }
+
+        public static void TryStackOvebrflow()
+        {
+            string animeUrl = "https://www.crunchyroll.com/de/fuuto-pi";
+            string downlaodHTMLPath = "c.html";
+
+            WebClient objWebClient = new WebClient();
+            Uri uriWebClient = new Uri(animeUrl);
+            objWebClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            //objWebClient.Headers.Add("Other;");
+            //objWebClient.Headers.Add("ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;");
+            //objWebClient.Headers.Add("Content-Type", "application / zip, application / octet - stream");
+            //objWebClient.Headers.Add("Accept-Encoding", "gzip,deflate,sdch");
+            //objWebClient.Headers.Add("Referer", "http://Something");
+            //objWebClient.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+            objWebClient.DownloadFile(uriWebClient, downlaodHTMLPath);
+        }
+        
+    }
+
+}
+
---
+++ RSSAnimeFeed_Console/RSSAnimeFeed_Console/Test_Libarie.cs
@@ -0,0 +1,94 @@
+﻿using SimpleFeedReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTML_Downlaod_Konsole
+{
+    public class RSSLibarie
+    {
+        /// <summary>
+        /// https://github.com/RobThree/SimpleFeedReader
+        /// Read RSS Recently Added Anime Videos from Crunchycroll
+
+
+        /// </summary>+
+        public void ReadNewAnimeTitleRss()
+        {
+            try
+            {
+                string rssUrl = "http://feeds.feedburner.com/crunchyroll/rss/anime?lang=deDE";
+                Console.WriteLine("Read rss from link: " + rssUrl);
+
+                FeedReader feedReader = new FeedReader();
+                IEnumerable<FeedItem> rssFeedItems = feedReader.RetrieveFeed(rssUrl);
+
+                // save readed anime titles
+                List<string> rssAnimeTitle = new List<string>();
+                foreach (FeedItem feedItem in rssFeedItems)
+                {
+                    //Console.WriteLine($"{feedItem.Date.ToString("g")}\t{feedItem.Title}");
+                    //Console.WriteLine(feedItem.Title);
+                    rssAnimeTitle.Add(feedItem.Title);    
+                }
+                viewListInConsole(rssAnimeTitle);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }             
+        }
+
+        public void viewListInConsole(List<string> value)
+        {
+            string patcher = "#######################################################################################################################";
+            Console.WriteLine("\n\t" + patcher);
+            foreach (string line in value)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine("\t" + patcher);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="weekday"></param>
+        /// <returns>return true if the weekday = the currebtlly day</returns>
+        public bool viewCalendaryWeek(string weekday)
+        {
+            DateOnly myDate = new DateOnly();
+            string weekDay = myDate.DayOfWeek.ToString();
+            string animeDay = weekday;
+            if (weekDay == animeDay)
+            {
+                Console.WriteLine("\n\t" + "Time to refresh the anime realse title list :) " + weekDay);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// create Threaf for checking every draiday for a new anime title 
+        /// </summary>
+        public void CreateThreadForRssAnimeList()
+        {
+            try
+            {
+                if (viewCalendaryWeek("Monday") == true)
+                {
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
